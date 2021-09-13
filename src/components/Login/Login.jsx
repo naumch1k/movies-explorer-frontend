b4134c60459025ac69359d450fb32783dab39481
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-09-13 17:49:45 -0500

    feat: add Login and Register components markup

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+
 function Login() {
-    return (
-        <p>Login component</p>
-    )
+  return (
+    <form className="form" name="login-form">
+      <h2 className="form__heading">Рады видеть!</h2>
+      <fieldset className="form__items">
+
+        <label class="form__label" for="email">E-mail</label>
+        <input 
+          className="form__item form__item_el_email"
+          id="email" 
+          type="email" 
+          name="email"
+          required 
+        />
+        <p className="form__error" id="email-error"></p>
+
+        <label class="form__label" for="password">Пароль</label>
+        <input 
+          className="form__item form__item_el_password"
+          id="password" 
+          type="password" 
+          name="password"
+          required 
+        />
+        <p className="form__error" id="password-error"></p>
+        
+      </fieldset>
+      <button className="form__submit-btn" type="submit">Войти</button>
+      <p className="form__text">
+        Ещё не зарегистрированы?
+        <Link className="form__link" to="/signup"> Регистрация</Link>
+      </p>
+    </form>
+  )
 }
 
 export default Login;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+
 function Navigation() {
-    return (
-        <p>Register component</p>
-    )
+  return (
+    <form className="form" name="signup-form">
+      <h2 className="form__heading">Добро пожаловать!</h2>
+      <fieldset className="form__items">
+
+        <label class="form__label" for="name">Имя</label>
+        <input 
+          className="form__item form__item_el_name"
+          id="name"
+          type="text"
+          name="name"
+          required
+        />
+        <p className="form__error" id="name-error"></p>
+
+        <label class="form__label" for="email">E-mail</label>
+        <input 
+          className="form__item form__item_el_email"
+          id="email"
+          type="email"
+          name="email"
+          required
+        />
+        <p className="form__error" id="email-error"></p>
+
+        <label class="form__label" for="password">Пароль</label>
+        <input 
+          className="form__item form__item_el_password"
+          id="password"
+          type="password"
+          name="password"
+          required
+        />
+        <p className="form__error" id="password-error"></p>
+
+      </fieldset>
+      <button className="form__submit-btn" type="submit">Зарегистрироваться</button>
+      <p className="form__text">
+        Уже зарегистрированы?
+        <Link to="/signin" className="form__link"> Войти</Link>
+      </p>
+    </form>
+  );
 }
 
 export default Navigation;
