1aa1b60519e720cc729e489d8c322bbfd3303651
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-09-25 10:06:12 -0500

    feat: add card's save button active styles

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,22 +1,44 @@
+import React from 'react';
 import './MoviesCard.css';
 
+import { ReactComponent as CheckIcon } from '../../images/check-icon.svg';
+
 function MoviesCard({ card }) {
-    return (
-      <li className="movies-card">
-        <img className="movies-card__image" src={card.thumbnail} alt={`Кадр из фильма ${card.name}`} />
-        <div className="movies-card__desc">
-          <h3 className="movies-card__title">{card.name}</h3>
-          <span className="movies-card__duration">{card.duration}</span>
-        </div>
-        <button
-          className="movies-card__save-btn"
-          type="button"
-          aria-label="Сохранить фильм"
-        >
-          Сохранить
-        </button>
-      </li>
-    )
-  }
+
+
+  // temp for dev purposes
+  const [isSaved, setIsSaved] = React.useState(false);
+
+  const handleSaveClick = () => {
+    setIsSaved(true);
+  };
+  // end
+
+  const cardSaveButtonClassName = (
+    `movies-card__save-btn ${isSaved ? 'movies-card__save-btn_active' : ''}`
+  ); 
+
+  const cardSaveButtonContent = (
+    isSaved ? <CheckIcon /> : 'Сохранить'
+  );
+
+  return (
+    <li className="movies-card">
+      <img className="movies-card__image" src={card.thumbnail} alt={`Кадр из фильма ${card.name}`} />
+      <div className="movies-card__desc">
+        <h3 className="movies-card__title">{card.name}</h3>
+        <span className="movies-card__duration">{card.duration}</span>
+      </div>
+      <button
+        className={cardSaveButtonClassName}
+        type="button"
+        aria-label="Сохранить фильм"
+        onClick={handleSaveClick}
+      >
+        {cardSaveButtonContent}
+      </button>
+    </li>
+  )
+}
   
-  export default MoviesCard;
+export default MoviesCard;
