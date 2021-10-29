f924dfe534a8116365159a805137ee1635102320
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-10-29 17:16:10 -0500

    fix grammar error

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -11,7 +11,7 @@ function Promo() {
         <div className="promo__main-content">
           <h1 className="promo__title">Welcome to Movies&nbsp;Explorer</h1>
           <p className="promo__text">The final project at Yandex.Practicum, Web Development program.</p>
-          <p className="promo__text">Movies Explorer is an online app that allows to look up movies and save them in your personal account.</p>
+          <p className="promo__text">Movies Explorer is an online app that allows to look up movies and save them to your personal account.</p>
           <Link
             className="promo__link"
             to={{ pathname: '#project' }}
